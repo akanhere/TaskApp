6986b9c8d11b4039e9379c0cefd8add5b27a6899
akanhere <ashwin.kanhere@gmail.com>
2020-02-09 23:58:02 +0530

    new changes for translation

--- TaskApp/TaskApp/Views/TaskListView.xaml.cs
+++ TaskApp/TaskApp/Views/TaskListView.xaml.cs
@@ -12,11 +12,42 @@ namespace TaskApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TaskListView : ContentPage
     {
+        double _pageHeight;
         public TaskListView()
         {
             InitializeComponent();
 
             BindingContext = new TaskListViewModel();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Popup.OnExpand += OnExpand;
+            Popup.OnCollapse += OnCollapse;
+        }
+
+        private void OnCollapse()
+        {
+            Popup.TranslateTo(0, _pageHeight - Popup.PageHeaderHeight, 500);
+        }
+
+        private void OnExpand()
+        {
+            var height = _pageHeight - Popup.PageHeaderHeight;
+            Popup.TranslateTo(0, Height - height, 500);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            Popup.OnExpand -= OnExpand;
+            Popup.OnCollapse -= OnCollapse;
+        }
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            _pageHeight = height;
+            Popup.TranslationY = _pageHeight - Popup.PageHeaderHeight;
+            base.OnSizeAllocated(width, height);
         }
     }
 }
---
+++ TaskApp/TaskApp/Views/TaskPopUpView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TaskApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TaskPopUpView : ContentView
+    {
+        public double PageHeaderHeight { get; private set; } = 80;
+        public delegate void TapHandler();
+        public TaskPopUpView()
+        {
+            InitializeComponent();
+        }
+        public TapHandler OnExpand { get; set; }
+        public TapHandler OnCollapse { get; set; }
+
+        private void OnExpandTapped(object sender, EventArgs args)
+        {
+            ToggleState("Expanded");
+            OnExpand?.Invoke();
+        }
+        private void OnCollapseTapped(object sender, EventArgs args)
+        {
+            ToggleState("Collapsed");
+            OnCollapse?.Invoke();
+        }
+        void ToggleState(string state)
+        {
+            VisualStateManager.GoToState(ExpandButton, state);
+            VisualStateManager.GoToState(CollapseButton, state);
+        }
+    }
+}
