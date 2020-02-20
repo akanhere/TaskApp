47369b59981fd9568c31ec14cbefeb712b9cafed
akanhere <ashwin.kanhere@gmail.com>
2020-02-20 11:22:21 +0530

    intermediate commit

---
+++ TaskApp/TaskApp/Common/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskApp.Common
+{
+    public enum TaskStatus
+    {
+        Open,
+        Assigned,
+        ClosedByReporter,
+        WaitingOnCustomer,
+        Expired,
+        Cancelled
+
+    }
+}
--- TaskApp/TaskApp/Models/Task.cs
+++ TaskApp/TaskApp/Models/Task.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TaskApp.Common;
 
 namespace TaskApp.Models
 {
@@ -9,5 +10,15 @@ namespace TaskApp.Models
         public string Name { get; set; }
         public string Category { get; set; }
         public DateTime AssignedDate { get; set; }
+        public TaskStatus Status { get; set; }
+
+        public string AssignedTo { get; set; }
+
+        public string ClosedBy { get; set; }
+
+        public DateTime AssignedOnDate { get; set; }
+        public DateTime ClosedOnDate { get; set; }
+        public string AssignedBy { get; set; }
+
     }
 }
